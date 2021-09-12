ad05dd6c880514d30365ff0a7d56730b2b4ea2ca
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-09-12 13:57:39 +0530

    Update animeall.js

--- plugins/animeall.js
+++ plugins/animeall.js
@@ -18,10 +18,21 @@ const eng = `💠● *ALL ANIME COMMANDS PLUGIN LIST* ●💠\n\n` +
 `⚙️Command: *.ranimel*\nℹ️Description: Random Anime Images.\n\n` +
 `⚙️Command: *.waifu*\nℹ️Description: Get random Waifu images.\n\n` + 
 `⚙️Command: *.neko*\nℹ️Description: Get random Neko images.\n\n` + 
+`⚙️Command: *.animeart*\nℹ️Description: Get random Anime Fan Art.\n\n` + 
 `💠● *NSFW* ●💠\n\n` + 
 `⚙️Command: *.nswaifu*\nℹ️Description: Get random NSFW Waifu images.\n\n` + 
 `⚙️Command: *.nsneko*\nℹ️Description: Get random NSFW Neko images.\n\n` + 
-`⚙️Command: *.hehe*\nℹ️Description: COMING SOON!\n\n`
+`⚙️Command: *.nsloli*\nℹ️Description: Get random NSFW Loli images.\n\n` + 
+`⚙️Command: *.nsloli2*\nℹ️Description: Get random NSFW Loli Type 2 images.\n\n` + 
+`⚙️Command: *.nsecchi*\nℹ️Description: Get random NSFW Ecchi images.\n\n` + 
+`⚙️Command: *.nshentai*\nℹ️Description: Get random NSFW Hentai images.\n\n` + 
+`⚙️Command: *.nscum*\nℹ️Description: Get random NSFW Cum images.\n\n` + 
+`⚙️Command: *.nstiddies*\nℹ️Description: Get random NSFW Big Tiddies images.\n\n` + 
+`⚙️Command: *.nsbooty*\nℹ️Description: Get random NSFW Booty images.\n\n` + 
+`⚙️Command: *.nsana*\nℹ️Description: Get random NSFW Anal images.\n\n` + 
+`⚙️Command: *.nspuss*\nℹ️Description: Get random NSFW Pussy images.\n\n` + 
+`⚙️Command: *.nsblo*\nℹ️Description: Get random NSFW Blow Job images.\n\n` + 
+`⚙️Command: *.nsall*\nℹ️Description: Get random NSFW All Types (any) images.\n\n`
 
 if (Config.LANG == 'EN') {
     if (Config.WORKTYPE == 'private') {
