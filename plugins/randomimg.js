b634e6e4eeafdf3345290b6edcf4b0f01accb790
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-09-12 13:52:19 +0530

    Update randomimg.js

--- plugins/randomimg.js
+++ plugins/randomimg.js
@@ -207,23 +207,13 @@ Amdi.applyCMD({pattern: 'nsblo ?(.*)', fromMe: LOL, dontAddCommandList: true,  d
 
 }));
 
-Amdi.applyCMD({pattern: 'nsnekogif ?(.*)', fromMe: LOL, dontAddCommandList: true,  deleteCommand: false}, (async (message, match) => {
+Amdi.applyCMD({pattern: 'nsall ?(.*)', fromMe: LOL, dontAddCommandList: true,  deleteCommand: false}, (async (message, match) => {
 
     var apikey = await QueenAmdi.api()
 
     var webimage = await axios.get('https://api.lolhuman.xyz/api/random2/nsfw_neko_gif?apikey=' + apikey.key, { responseType: 'arraybuffer' })
 
-    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.gif, {mimetype: Mimetype.gif, caption: Config.CAP, quoted: message.data, thumbnail: thumb })
-
-}));
-
-Amdi.applyCMD({pattern: 'nshengif ?(.*)', fromMe: LOL, dontAddCommandList: true,  deleteCommand: false}, (async (message, match) => {
-
-    var apikey = await QueenAmdi.api()
-
-    var webimage = await axios.get('https://api.lolhuman.xyz/api/random2/random_hentai_gif?apikey=' + apikey.key, { responseType: 'arraybuffer' })
-
-    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.gif, {mimetype: Mimetype.gif, caption: Config.CAP, quoted: message.data, thumbnail: thumb })
+    await message.client.sendMessage(message.jid,Buffer.from(webimage.data), MessageType.video, {mimetype: Mimetype.gif, caption: Config.CAP, quoted: message.data, thumbnail: thumb })
 
 }));
 
