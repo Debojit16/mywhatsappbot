a464b9aa372cf24822e946fb4f7f13372819ea81
debojit16mitra <33363581+debojit16mitra@users.noreply.github.com>
2021-09-15 16:14:51 +0530

    Update aichat.js

--- plugins/aichat.js
+++ plugins/aichat.js
@@ -25,11 +25,11 @@ if (Config.CHAT_BOT == 'true') {
 	if (Config.WORKTYPE == 'private') {
 		Amdi.applyCMD({pattern: 'bot ?(.*)', fromMe: true, desc: Lang.BOT_DESC, deleteCommand: false}, async (message, match) => {
 			if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
-			const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
+			const url = `https://api.simsimi.net/v2/?text=${match[1]}&lang=en&cf=true`;
 			try {
 				const response = await got(url);
 				const json = JSON.parse(response.body);
-			if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n*👸🏻 ' + Lang.BOT +'* ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
+			if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n*👸🏻 ' + Lang.BOT + json.messages[0].response\n\n' , MessageType.text,{quoted: message.data});
 			} catch {
 				return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
 			}
@@ -39,11 +39,11 @@ if (Config.CHAT_BOT == 'true') {
 	else if (Config.WORKTYPE == 'public') {
 		Amdi.applyCMD({pattern: 'bot ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
 			if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
-			const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
+			const url = `https://api.simsimi.net/v2/?text=${match[1]}&lang=en&cf=true`;
 			try {
 				const response = await got(url);
 				const json = JSON.parse(response.body);
-			if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n*👸🏻 ' + Lang.BOT +'* ```' + json.messages[0].response + '```\n\n' , MessageType.text,{quoted: message.data});
+			if (response.statusCode === 200) return await message.client.sendMessage(message.jid, '\n*👸🏻 ' + Lang.BOT + json.messages[0].response\n\n' , MessageType.text,{quoted: message.data});
 			} catch {
 				return await message.client.sendMessage(message.jid, Lang.NOT_FOUNDAC, MessageType.text);
 			}
@@ -55,7 +55,7 @@ else if (Config.CHAT_BOT == 'false') {
 	if (Config.WORKTYPE == 'private') {
 		Amdi.applyCMD({pattern: 'bot ?(.*)', fromMe: true, desc: Lang.BOT_DESC, deleteCommand: false}, async (message, match) => {
 			if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
-			const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
+			const url = `https://api.simsimi.net/v2/?text=${match[1]}&lang=en&cf=true`;
 			try {
 				const response = await got(url);
 				const json = JSON.parse(response.body);
@@ -69,7 +69,7 @@ else if (Config.CHAT_BOT == 'false') {
 	else if (Config.WORKTYPE == 'public') {
 		Amdi.applyCMD({pattern: 'bot ?(.*)', fromMe: false, desc: Lang.BOT_DESC}, async (message, match) => {
 			if (match[1] === 'xx') return await message.reply(Lang.NEED_LOCATIONA);
-			const url = `https://api.simsimi.net/v1/?text=${match[1]}&lang=en&cf=true`;
+			const url = `https://api.simsimi.net/v2/?text=${match[1]}&lang=en&cf=true`;
 			try {
 				const response = await got(url);
 				const json = JSON.parse(response.body);
